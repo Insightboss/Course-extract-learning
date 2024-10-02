34793ae8fa2c8d47a0baae1c2b9aeb229b3c3c59
Insightboss <nrfxbqm574@safeinbox.xyz>
2024-10-02 13:43:16 +0530

    Update vars.py
    Ind

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "24763783"
-api_hash = "0534b1088b5197e703895f37fb78d311"
-bot_token = ""
+api_id = "27774831"
+api_hash = "2bcc9f4ef69859378ecbb2ba0ce80d0e"
+bot_token = "AAF8MoVdKJ6KeYCHNx0gGbtCBEfxz1VniME"
